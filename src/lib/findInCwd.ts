457f6c0f90357ad311234ab274aa8f9c08c1dbbd
Alessandro Falezza <alessandro.falezza@intesys.it>
2019-04-02 14:39:53 +0200

    fixed env path

--- src/lib/findInCwd.ts
+++ src/lib/findInCwd.ts
@@ -2,14 +2,14 @@ import Path from "path";
 import fs from "fs";
 
 /**
- * Searches for files in current working diractory, returns the first file found.
+ * Searches for files in current working directory, returns the first file found.
  * String returned is the full file path
  *
  * @param files string[]
- * @returns string | undefined full path to found file
+ * @returns string | undefined full path to file
  */
-const findInCwd = (files: string[]): string | undefined =>
-  files.find(file => {
+const findInCwd = (files: string[]): string | undefined => {
+  const file = files.find(file => {
     try {
       const path = Path.join(process.cwd(), file);
       const stat = fs.statSync(path);
@@ -18,5 +18,11 @@ const findInCwd = (files: string[]): string | undefined =>
       return false;
     }
   });
+
+  if (file) {
+    return Path.join(process.cwd(), file);
+  }
+  return;
+};
 
 export default findInCwd;
