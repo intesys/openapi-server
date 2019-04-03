aed7af3f4cbad061edece6b9961bbb7ee3ac8c8d
Alessandro Falezza <alessandro.falezza@intesys.it>
2019-04-03 16:02:33 +0200

    fixed 404 when POST response is empty

--- src/middlewares/sendBody.ts
+++ src/middlewares/sendBody.ts
@@ -1,8 +1,8 @@
 import { RequestHandler } from "express";
-import _ from 'lodash';
+import _ from "lodash";
 
 const sendBody = (): RequestHandler => (req, res, next) => {
-  if (_.get(res, 'locals.body')) {
+  if (!_.isUndefined(_.get(res, "locals.body"))) {
     return res.send(res.locals.body);
   }
   return next();
--- src/middlewares/tryProxy.ts
+++ src/middlewares/tryProxy.ts
@@ -1,18 +1,25 @@
-import { RequestHandler } from 'express';
-import voidMiddleware from './void';
-import proxy from '../lib/proxy';
-import _ from 'lodash';
-import { PROXY_PROTOCOL, PROXY_HOSTNAME, PROXY_PORT, PROXY_PREFIX, proxyUrl } from '../lib/globals';
-
-
+import { RequestHandler } from "express";
+import voidMiddleware from "./void";
+import proxy from "../lib/proxy";
+import _ from "lodash";
+import {
+  PROXY_PROTOCOL,
+  PROXY_HOSTNAME,
+  PROXY_PORT,
+  PROXY_PREFIX,
+  proxyUrl
+} from "../lib/globals";
 
 /**
  * @param method one value of `operations`
  * @param route path key, as example: '/item/{id}'
  */
-const tryProxy = (method: string, route: string): RequestHandler => (req, res, next) => {
-
-  if (_.get(res, 'locals.body')) {
+const tryProxy = (method: string, route: string): RequestHandler => (
+  req,
+  res,
+  next
+) => {
+  if (!_.isUndefined(_.get(res, "locals.body"))) {
     return voidMiddleware(req, res, next);
   }
 
