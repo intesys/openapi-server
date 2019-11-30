ef5b572537adedfee58335915338bba3ea569141
Alessandro Falezza <alessandro.f@ideaedi.it>
2019-12-01 00:24:19 +0100

    Move default env variables to VSC debug

--- src/config.ts
+++ src/config.ts
@@ -2,24 +2,24 @@ import { isBoolean } from "lodash";
 import Env from "./types/env";
 
 export const defaults: Env = {
-  API_YML: "/examples/api.yml,/examples/api",
+  API_YML: "api.yaml",
   API_PREFIX: "/api",
   API_PORT: "3000",
   API_PROTOCOL: "http",
   API_HOSTNAME: "localhost",
 
   RESOURCES: false,
-  RESOURCES_PREFIX: "/examples/resources",
+  RESOURCES_PREFIX: "/resources",
 
   MOCKS: true,
-  MOCKS_PATH: "/examples/mocks",
+  MOCKS_PATH: "/mocks",
 
   PROXY: false,
-  PROXY_PROTOCOL: "http",
-  PROXY_HOSTNAME: "localhost",
-  PROXY_PORT: "3001",
-  PROXY_PREFIX: "/api",
-  PROXY_RESOURCES_PREFIX: "/resources",
+  PROXY_PROTOCOL: "",
+  PROXY_HOSTNAME: "",
+  PROXY_PORT: "",
+  PROXY_PREFIX: "",
+  PROXY_RESOURCES_PREFIX: "",
 
   SKIP_VALIDATION: false,
   LOG: false,
