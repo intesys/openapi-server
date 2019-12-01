150814681c9537a11526c546ee0bf4d94393b5ac
Alessandro Falezza <alessandro.f@ideaedi.it>
2019-12-01 01:03:50 +0100

    Debug v3 router

--- src/routes/v3.ts
+++ src/routes/v3.ts
@@ -1,3 +1,11 @@
+/**
+ * By design this lib takes only the first server
+ * and uses it's path to prefix routes.
+ *
+ * If proxy is enabled, proxy configuration is used
+ * instead of server described in spec.
+ */
+
 import { RequestHandler, Router } from "express";
 import { get } from "lodash";
 import { OpenAPIV3 } from "openapi-types";
@@ -5,46 +13,48 @@ import toExpressParam from "../lib/toExpressParam";
 import { operations } from "../routes";
 import getMiddlewares from "./middlewares";
 
+const compileServerVars = (
+  url: string,
+  variables: {
+    [variable: string]: OpenAPIV3.ServerVariableObject;
+  }
+): string => {
+  const mapObj = {};
+  Object.keys(variables).map(item => {
+    return (mapObj["{" + item + "}"] = variables[item].default);
+  });
+  var regex = new RegExp("(" + Object.keys(mapObj).join("|") + ")", "gi");
+  const newUrl = url.replace(regex, function(matched) {
+    return mapObj[matched];
+  });
+  return newUrl;
+};
+
+const getServer = (servers: OpenAPIV3.ServerObject[]): string => {
+  const server = servers[0];
+  const variables = get(server, "variables");
+  const url = get(server, "url");
+  if (variables) {
+    return compileServerVars(url, variables);
+  }
+  return url;
+};
+
+const getPath = (server: string): string => {
+  const url = new URL(server);
+  return url.pathname;
+};
+
 const buildV3Routes = (
   router: Router,
   paths: Record<string, OpenAPIV3.PathItemObject>,
   servers: OpenAPIV3.ServerObject[]
 ): Router => {
-  const getServerUrlWithoutPlaceholder = (
-    servers: OpenAPIV3.ServerObject[]
-  ) => {
-    const variables = get(servers[0], "variables");
-    const url = get(servers[0], "url");
-    if (variables) {
-      const mapObj = {};
-      Object.keys(variables).map(item => {
-        return (mapObj["{" + item + "}"] = variables[item].default);
-      });
-      var regex = new RegExp("(" + Object.keys(mapObj).join("|") + ")", "gi");
-      const newUrl = url.replace(regex, function(matched) {
-        return mapObj[matched];
-      });
-      return newUrl;
-    }
-    return url;
-  };
-
-  const getRoute = (route: string, servers: OpenAPIV3.ServerObject[]) => {
-    // TODO: IMPORTANTE!! da decidere cosa fare con il parametro servers nei file v3
-    // if (servers.length > 0) {
-    //   // TODO: multi url da gestire in futuro
-    //   const url = new URL(getServerUrlWithoutPlaceholder(servers));
-    //   // TODO: "host" e "schemes" da gestire eventualmente in futuro
-    //   // const host = url.host;
-    //   // const schemes = url.protocol.replace(/:/g, "");
-    //   const basePath = url.pathname;
-    //   return basePath + route;
-    // }
-    return route;
-  };
-
+  const server = getServer(servers);
+  const basePath = getPath(server);
   Object.keys(paths).forEach((route: string) => {
-    const expressRoute = toExpressParam(getRoute(route, servers));
+    const fullRoute = basePath + route;
+    const expressRoute = toExpressParam(fullRoute);
     const routerRef = router.route(expressRoute);
     const methods = Object.keys(operations);
     const spec: OpenAPIV3.PathItemObject = paths[route];
@@ -53,7 +63,7 @@ const buildV3Routes = (
       const operationSpec: OpenAPIV3.OperationObject = spec[method];
       const middlewares: RequestHandler[] = getMiddlewares(
         method,
-        route,
+        fullRoute,
         operationSpec
       );
       routerRef[method](middlewares);
