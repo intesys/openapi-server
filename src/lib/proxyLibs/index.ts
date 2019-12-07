e024e2920af0cb6704800a962e063e168b03c736
Alessandro Falezza <alessandro.f@ideaedi.it>
2019-12-07 14:45:14 +0100

    Disable temporary got lib

--- src/lib/proxyLibs/index.ts
+++ src/lib/proxyLibs/index.ts
@@ -1,9 +1,9 @@
 import AxiosLib from "./axios";
 import PostmanRequestLib from "./postmanRequest";
-import GotLib from "./got";
+// import GotLib from "./got";
 
 export const proxyLibs = {
   axios: AxiosLib,
   "postman-request": PostmanRequestLib,
-  got: GotLib,
+  // got: GotLib,
 };
