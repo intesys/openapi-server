3da712c0e423f854b07a5e1944fbd8e57342a3c5
Henrique Ribeiro <henrique.ribeiro@intesys.it>
2019-12-09 15:25:30 +0100

    Fix postman request call

--- src/lib/proxyLibs/postmanRequest.ts
+++ src/lib/proxyLibs/postmanRequest.ts
@@ -2,16 +2,27 @@
 import request from "postman-request";
 import { ProxyLib } from "../../types/proxyLib";
 import { RemoteError } from "../proxy";
+import url from "url";
 
-const PostmanRequestLib: ProxyLib = (method, url, headers = {}) => async (req, res) => {
+const PostmanRequestLib: ProxyLib = (method, rawUrl, headers = {}) => async (req, res) => {
+  const temp = {
+    Authorization: headers.authorization,
+  };
+
   try {
-    return request({
-      method,
-      headers,
-      url,
-      body: req.body,
-      strictSSL: false,
-    });
+    return request(
+      {
+        method,
+        headers: temp,
+        url: url.parse(rawUrl),
+        // body: req.body,
+        strictSSL: false,
+      },
+      (error: {}, responseCode: {}, body: {}) => {
+        console.log("error", error);
+        console.log("body", body);
+      }
+    );
   } catch (err) {
     throw new RemoteError(`${method} ${url}`, err);
   }
