a4b9ff756d2d92e09ddd7e9fdbd21cc66cda425e
Alessandro Falezza <afalezza@intesys.it>
2019-12-11 11:58:58 +0100

    Reset proxy.ts

--- src/lib/proxy.ts
+++ src/lib/proxy.ts
@@ -23,28 +23,13 @@ export default (url: string): RequestHandler => async (req, res, next) => {
     const fullUrl = `${url}${req.url}`;
     const headers = req.headers;
     const method = req.method.toLowerCase() as method;
-
     const response = await proxyLibinstance(method, fullUrl, headers)(req, res);
-
     res.locals.body = response.data;
-
-    // On postman app, it works only if this set is not made. although it goes with weird headers.
-    // Note that these headers inside response are pretty similar to the ones that appear when a
-    // direct call to the gateway is done, so I believe the problem is not on them. Take a look
-    // at postmanRequest.ts
-
-    // res.set(response.headers);
-
+    res.set(response.headers);
     res.set("Forwarded", `for=${url}`);
-    res.set("Content-Type", response.headers["content-type"]);
-    res.set("Id", response.headers["id"]);
-    res.set("AuthToken", response.headers["authtoken"]);
-    res.set("Authorization", response.headers["authorization"]);
-    // Here headers are being set manually because passing the whole load of response headers directly
-    // just like in the commented line of code above causes unexpected issues
-
     log({
       "Request forwarded to": `${method.toUpperCase()} ${fullUrl}`,
+      "Reponse headers": response.headers,
       "Request body": req.body,
       "Response body": response.data,
     });
