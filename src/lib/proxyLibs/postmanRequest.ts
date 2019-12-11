7bc9104eeec0767534e225efe6291eb0679bddcb
Alessandro Falezza <afalezza@intesys.it>
2019-12-11 11:59:23 +0100

    Try filter headers (req and res)

--- src/lib/proxyLibs/postmanRequest.ts
+++ src/lib/proxyLibs/postmanRequest.ts
@@ -3,17 +3,18 @@ import request from "postman-request";
 import { ProxyLib } from "../../types/proxyLib";
 import { RemoteError } from "../proxy";
 import url from "url";
+import { filterHeaders } from "./utils";
 
 const PostmanRequestLib: ProxyLib = (method, rawUrl, headers = {}) => async (req, res) => {
   // If the parameter 'header' is passed directly to request(), it takes back garbage code
   // in the body instead of a json with preventivo data
-  const temp = {
-    Authorization: headers.authorization,
-  };
+  // const temp = {
+  //   Authorization: headers.authorization,
+  // };
 
   const options = {
     method,
-    headers: temp,
+    headers: filterHeaders(headers),
     url: url.parse(rawUrl),
     ...(Object.keys(req.body).length != 0 && { body: JSON.stringify(req.body) }),
     strictSSL: false,
@@ -24,7 +25,7 @@ const PostmanRequestLib: ProxyLib = (method, rawUrl, headers = {}) => async (req
       if (error) {
         return reject(new RemoteError(`${method} ${url}`, error));
       }
-      resolve({ data: body, headers: response.headers });
+      resolve({ data: body, headers: filterHeaders(response.headers) });
     });
   });
 };
