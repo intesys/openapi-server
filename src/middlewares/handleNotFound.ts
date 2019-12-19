db663b9d97d507a23a73be0005ba876d62605eb7
Alessandro Falezza <afalezza@intesys.it>
2019-12-19 18:22:56 +0100

    Update not found message

--- src/middlewares/handleNotFound.ts
+++ src/middlewares/handleNotFound.ts
@@ -4,8 +4,8 @@ import { MOCKS, PROXY, MOCKS_PATH, proxyUrl } from "../lib/globals";
 export default (): RequestHandler => (req, res, next) => {
   const messages = [];
   console.log(req.url);
-  MOCKS && messages.push(`mock in ${MOCKS_PATH}`);
-  PROXY && messages.push(`remote ${proxyUrl}`);
-  const notFoundMessage = `Uri ${req.url} not found: ${messages.join(", ")}.`;
+  MOCKS && messages.push(`- Mock not found in: ${MOCKS_PATH}`);
+  PROXY && messages.push(`- Remote host called: ${proxyUrl}`);
+  const notFoundMessage = `Uri ${req.url} not found \n${messages.join("\n")}`;
   res.status(404).send(notFoundMessage);
 };
