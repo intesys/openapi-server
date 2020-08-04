b43d29e57eacba2c1a2fea99ae662ab50c4fd243
Alessandro Falezza <alessandro.falezza@intesys.it>
2020-08-04 18:56:09 +0200

    Update dependencies -  remove tslint for eslint - remove prettierrc

--- src/lib/load.ts
+++ src/lib/load.ts
@@ -1,26 +1,30 @@
 import fs from "fs";
 import yaml from "js-yaml";
-import { OpenAPI } from "openapi-types";
-import JsonRefs from "json-refs";
+import JsonRefs, { JsonRefsOptions } from "json-refs";
 import resolveAllOf from "json-schema-resolve-allof";
+import { OpenAPI } from "openapi-types";
+
+interface SuperagentResponseMock {
+  text: string;
+}
 
 export default async (file: string): Promise<OpenAPI.Document> => {
   const spec: OpenAPI.Document = await loadYaml(file);
-  let resolvedSpec: any = await resolve(spec);
+  let resolvedSpec = await resolve(spec);
   resolvedSpec = resolveAllOf(resolvedSpec);
-  return resolvedSpec as OpenAPI.Document;
+  return resolvedSpec;
 };
 
 const loadYaml = async (file: string): Promise<OpenAPI.Document> => {
-  return new Promise((resolve: Function, reject: Function) => {
+  return new Promise((_resolve, reject) => {
     fs.readFile(file, "utf8", (err, data) => {
       if (err) {
         return reject(new Error(`Unable to load file ${file}`));
       }
       try {
-        const spec: OpenAPI.Document = yaml.safeLoad(data);
-        resolve(spec);
-      } catch (err) {
+        const spec: OpenAPI.Document = yaml.safeLoad(data) as OpenAPI.Document;
+        _resolve(spec);
+      } catch (_err) {
         reject(new Error(`Error reading file ${file}`));
       }
     });
@@ -28,13 +32,16 @@ const loadYaml = async (file: string): Promise<OpenAPI.Document> => {
 };
 
 const resolve = async (spec: OpenAPI.Document): Promise<OpenAPI.Document> => {
-  const options = {
+  const options: JsonRefsOptions = {
     loaderOptions: {
-      processContent: function(res: any, callback: Function) {
-        callback(null, yaml.safeLoad(res.text));
-      },
-    },
+      processContent: function(
+        res: SuperagentResponseMock,
+        callback: (str: string) => void
+      ) {
+        callback(yaml.safeLoad(res.text) as string);
+      }
+    }
   };
-  const res: any = await JsonRefs.resolveRefs(spec, options);
+  const res = await JsonRefs.resolveRefs(spec, options);
   return res.resolved as OpenAPI.Document;
 };
