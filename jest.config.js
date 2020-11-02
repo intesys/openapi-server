545bf12300cf112e4d909682a731554cacdfa5ce
Alessandro Falezza <alessandro.falezza@intesys.it>
2020-11-02 21:34:44 +0100

    Simplify test config and enable strict typescript

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,9 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+.(?!js|jsx|ts|tsx).*$": "jest-transform-stub", // stubs all not javascript files
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  preset: "ts-jest",
   globals: {
     "ts-jest": {
       tsConfig: "tsconfig.test.json",
-      diagnostics: {
-        warnOnly: true,
-      },
     },
   },
-  collectCoverageFrom: ["src/**/*.{js,jsx}", "!src/generated/**"],
-  coverageDirectory: "./coverage",
-  testEnvironment: "node",
 };
---
+++ src/types/mock-express-request.d.ts
@@ -0,0 +1,1 @@
+declare module "mock-express-request";
