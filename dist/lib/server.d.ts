29f6fe3a1f6477d0d3438369875389170b29b6c2
Zeno Begnini <zeno.begnini@intesys.it>
2026-02-05 09:27:59 +0100

    chore(deps): keep @types/node to v20 for stability

--- dist/lib/server.d.ts
+++ dist/lib/server.d.ts
@@ -1,4 +1,5 @@
+import https from "https";
 import http from "http";
 import { HTTPProtocol } from "../types/env";
 import { Express } from "express";
-export declare const createServer: (protocol: HTTPProtocol, app: Express) => http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
+export declare const createServer: (protocol: HTTPProtocol, app: Express) => https.Server<typeof http.IncomingMessage, typeof http.ServerResponse> | http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
